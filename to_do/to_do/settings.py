506c35d84ea2b4821faa3a71964897bab7731226
adhilna <adhilchy11@gmail.com>
2025-04-11 18:22:27 +0530

    Added user registration with JWT login functionality

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'Home',
     'rest_framework',
     'corsheaders',
+    'Accounts',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
