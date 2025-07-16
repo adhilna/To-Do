a9ba396d5973a7612d9a1ad7094cb7bb226a3215
adhilna <adhilchy11@gmail.com>
2025-07-16 18:07:33 +0530

    Initial commit for celery and beat deployment

--- to_do/to_do/celery.py
+++ to_do/to_do/celery.py
@@ -2,7 +2,6 @@ import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'to_do.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'to_do.settings')
 app = Celery('to_do')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
